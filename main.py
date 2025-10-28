5246fbbb141ff2d4016d365f993ee732d8e3016d
Narendhar Tangella <narismailbox@gmail.com>
2025-10-28 15:34:39 +0000

    Updated the main.py for github actions

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ app = FastAPI()
  
 @app.get("/") 
 def read_root(): 
-    return {"message": "Hello from FastAPI on Cloud Run!"} 
+    return {"message": "Hello from FastAPI on Cloud Run! - via Github actions"} 
